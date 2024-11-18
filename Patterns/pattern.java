b1845e65ac08cdc15780bb754aa647137c3511e6
bhushan354 <deshmukhumesh354@gmail.com>
2024-11-18 08:12:14 +0530

    Initial commit

---
+++ Patterns/pattern.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class pattern {
+
+    // print matrix
+
+    // public static void main(String args[]) {
+    //   Scanner scanner = new Scanner(System.in);
+    //   System.out.println("Enter a number");
+    //   int n = scanner.nextInt();
+    //   System.out.println("Enter another number");
+    //   int m = scanner.nextInt();
+
+    //   for (int i = 0; i < m; i ++) {
+    //     for (int j =  1; j <= n; j++) {
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    //   }
+
+    // }
+
+
+    
+
+    
+}
