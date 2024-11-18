034e6ee3ee03a1683225faeae477eaa460630270
bhushan354 <deshmukhumesh354@gmail.com>
2024-11-18 08:28:40 +0530

    pattern problem 2

--- Patterns/pattern.java
+++ Patterns/pattern.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class pattern {
 
+    
     // print matrix
 
     // public static void main(String args[]) {
@@ -22,7 +23,57 @@ public class pattern {
     // }
 
 
+    // output
+
+    // Enter a number
+    // 6
+    // Enter another number
+    // 4
+    // ******
+    // ******
+    // ******
+    // ******
     
 
+    // *****************************************************************************************************************
+
+
+    // star on only outer side
+
+    // public static void main(String args[]) {
+    //   Scanner scanner = new Scanner(System.in);
+    //   System.out.println("Enter a number");
+    //   int n =  scanner.nextInt();;
+    //   System.out.println("Enter another number");
+    //   int m =  scanner.nextInt();;
+
+    //   for (int i = 0; i < n; i++) {
+    //     for (int j =  0; j < m; j++) {
+    //        if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+    //        System.out.print("*");
+    //        }else {
+    //         System.out.print(" ");
+    //        }
+    //     }
+    //     System.out.println();
+    //   }
+
+    // }
+
+    // output
+
+    // Enter a number
+    // 6
+    // Enter another number
+    // 4
+    // ****
+    // *  *
+    // *  *
+    // *  *
+    // *  *
+    // ****
+
+
+    // *****************************************************************************************************************
     
 }
