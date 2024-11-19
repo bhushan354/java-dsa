df2ff9c0bf401fd3ab9791b99c8e6bb81126d0ef
bhushan354 <deshmukhumesh354@gmail.com>
2024-11-19 15:57:10 +0530

    new question today of pattern

--- Patterns/patterns2.java
+++ Patterns/patterns2.java
@@ -69,4 +69,34 @@ public class patterns2 {
     //   *** 
     //  **** 
     // ***** 
+
+ // *****************************************************************************************************************
+
+ //numbers upside down
+    // public static void main (String args[]) {
+      
+    //     Scanner scanner = new Scanner(System.in);
+    //     System.out.println("Enter a number");
+    //     int n = scanner.nextInt();
+
+    //     for (int i = n; i > 0; i--) {
+
+    //        for (int j = 1; j <= i; j++) {
+
+    //         System.out.print(j + "");
+    //        }
+    //        System.out.println();
+    //     }
+    // }
+
+
+    // output 
+    // Enter a number
+    // 5
+    // 12345
+    // 1234
+    // 123
+    // 12
+    // 1
+    
 }
