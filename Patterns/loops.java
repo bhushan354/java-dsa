afe56bdf749c47b366a85203493691447efc57eb
bhushan354 <deshmukhumesh354@gmail.com>
2024-12-08 16:28:00 +0530

    loops problems

---
+++ Patterns/loops.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class loops {
+
+    // public static void main(String args[]) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     int n = scanner.nextInt();
+    //     for (int i = 1; i <= n; i++) {
+    //         System.out.println(i);
+    //     }
+    // }
+
+
+    // public static void main(String args[]) {
+    //     Scanner scanner = new Scanner(System.in);
+    //     int n = scanner.nextInt();
+
+    //     for (int i = 1; i <= n; i++) {
+
+    //         if(i % 2 == 0) {
+    //            System.out.println(i);
+    //         }
+    //     }
+    // }
+
+
+    //  public static void main(String args[]) {
+    //     Scanner scanner = new Scanner(System.in);
+       
+    //     int n = scanner.nextInt();
+
+    //     int evenCount = 0;
+    //     int oddCount = 0;
+    //     int positveCount = 0;
+    //     int negativeCount = 0;
+
+    //     for (int i = 0; i <= n; i++) {
+    //         int m = scanner.nextInt();
+
+    //         if (m % 2 == 0) {
+    //             evenCount++;
+    //         } else {
+    //             oddCount++;
+    //         }
+
+    //         if (m > 0) {
+    //             positveCount++;
+    //         } else {
+    //             negativeCount++;
+    //         }
+    //     }
+
+    //     System.out.println("Even :" + evenCount);
+    //     System.out.println("Odd :" + oddCount);
+    //     System.out.println("Positive :" + positveCount);
+    //     System.out.println("Negative :" + negativeCount);
+        
+    // }
+
+
+}
